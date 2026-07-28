d2cae62715ffb0aedad05e31b6f40515dba620d4
CY <cycleapple@gmail.com>
2026-07-28 20:56:17 +0800

    Map Taiwan CHT language to TC Excel pages

--- ExcelSheetExtensions.cs
+++ ExcelSheetExtensions.cs
@@ -9,14 +9,7 @@ public static class ExcelSheetExtensions
 {
     /// <summary>Return a language backed by pages in the TW client's Excel data.</summary>
     public static ClientLanguage GetSafeLanguage(this IDataManager dataManager)
-        => dataManager.Language switch
-        {
-            ClientLanguage.Japanese or
-            ClientLanguage.English or
-            ClientLanguage.German or
-            ClientLanguage.French => dataManager.Language,
-            _                     => ClientLanguage.English,
-        };
+        => NormalizeLanguage(dataManager.Language);
 
     /// <summary>Load a sheet using English when the TW client reports its unsupported legacy language slot.</summary>
     public static ExcelSheet<T> GetSafeExcelSheet<T>(
@@ -26,15 +19,22 @@ public static class ExcelSheetExtensions
         where T : struct, IExcelRow<T>
     {
         var actualLanguage = language ?? dataManager.GetSafeLanguage();
-        actualLanguage = actualLanguage switch
-        {
-            ClientLanguage.Japanese or
-            ClientLanguage.English or
-            ClientLanguage.German or
-            ClientLanguage.French => actualLanguage,
-            _                     => ClientLanguage.English,
-        };
+        actualLanguage = NormalizeLanguage(actualLanguage);
 
         return dataManager.GetExcelSheet<T>(actualLanguage, name);
     }
+
+    private static ClientLanguage NormalizeLanguage(ClientLanguage language)
+        => language switch
+        {
+            ClientLanguage.ChineseTraditional => ClientLanguage.TraditionalChinese,
+            ClientLanguage.Japanese or
+            ClientLanguage.English or
+            ClientLanguage.German or
+            ClientLanguage.French or
+            ClientLanguage.ChineseSimplified or
+            ClientLanguage.Korean or
+            ClientLanguage.TraditionalChinese => language,
+            _                                 => ClientLanguage.TraditionalChinese,
+        };
 }
